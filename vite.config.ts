cdb4d5f5367e6d01cd7558c5d32e9b1927aa3fe4
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2025-07-10 13:25:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-weather-app/',
+  base: '/a-vue-weather-app/',
   plugins: [
     vue(),
     vueJsx(),
